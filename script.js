8051c165264f072d15d0f84e336312bce1f04a1a
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-06-04 01:20:00 +0530

    not at all

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let emoji1 = document.getElementById("emoji1");
-let emoji2 = document.getElementById("emoji2");
-
-let emoji3 = document.getElementById("emoji3");
-
-console.log(emoji1.innerText);
+let emoji=document.querySelectorAll(".emoji");
+let array = Array.from(emoji);
+for(let i=0;i<array.length;i++){
+    addEventListener("click",function(){
+        let select = array[i].innerText;
+        console.log(select);
+    })
+}
 
