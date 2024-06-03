c811a5f3e7d5d9ca843df664feb2203a7606de82
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-06-03 23:48:27 +0530

    not done

---
+++ script.js
@@ -0,0 +1,7 @@
+let emoji1 = document.getElementById("emoji1");
+let emoji2 = document.getElementById("emoji2");
+
+let emoji3 = document.getElementById("emoji3");
+
+console.log(emoji1.innerText);
+
